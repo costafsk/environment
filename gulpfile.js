89b7576a27041af5cc1265238a26a5d1293c34fc
Diego Costa <diego.machado.d.costa@gmail.com>
2019-08-06 00:39:07 +0000

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gulp = require('gulp');
+
+
+// Write your tasks
+
+
+gulp.task('default', () => {
+    // gulp.watch...
+});
+
+
+// More details -> https://github.com/costafsk/patrimonio/blob/master/gulpfile.js
