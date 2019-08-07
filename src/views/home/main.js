c927af33936af120402a13ad0b72482188cc6fe9
Diego Costa <diego.machado.d.costa@gmail.com>
2019-08-07 03:23:35 +0000

    +slider

--- src/views/home/main.js
+++ src/views/home/main.js
@@ -1,1 +1,18 @@
-console.log('main!');
+document.addEventListener('DOMContentLoaded', () => init());
+
+const init = () => {
+    const carousel = document.querySelector('.carousel');
+    
+    const instance = M.Carousel.init(carousel, {
+        fullWidth: true,
+        indicators: true
+    });
+
+    const setHeight = () => carousel.style.height = `${window.innerHeight}px`;
+
+    // Setting height (init);
+    setHeight();
+
+    // Setting height (browser resizing);
+    document.body.onresize = () => setHeight();
+}
